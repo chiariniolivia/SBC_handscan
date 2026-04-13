47e3895e653df10124a0ac1fe0bd5a878eae6145
Olivia Chiarini <chiariniolivia@gmail.com>
2026-04-13 12:11:12 -0500

    fixed earliest frame finding

--- handscan.py
+++ handscan.py
@@ -16,9 +16,9 @@ from sbcbinaryformat import Streamer, Writer
 parser = argparse.ArgumentParser(description="A program to assist with handscanning for the SBC. Make sure you are in the conda enviorment from LAr10Ana.")
 parser.add_argument("-e", "--event", required=True,help="The name of the event you want to analyze, assumed to be in SBC-25-daqdata")
 parser.add_argument("-s", "--scratch", required=False, default='.', help="Directory to do scratch work in i.e. untar data")
-parser.add_argument("-l", "--log", required=False, type=bool, default=True, help="Bool to print debug messages")
-parser.add_argument("-i", "--index", required=False, default=0, help="Guess at index when bubble appears")
-parser.add_argument("-r", "--recon", required=False, default=False, help="Bool to stop after grabbing reconsuctrion guesses.")
+parser.add_argument("-i", "--index", required=False, default=-1, help="Guess at index when bubble appears")
+parser.add_argument("-r", "--recon", default=False,required=False,action="store_true", help="Flag to stop after grabbing reconsuctrion guesses.")
+parser.add_argument("-l", "--log", default=False, required=False,action="store_true", help="Flag to print debug messages")
 
 args = parser.parse_args()
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -45,7 +45,6 @@ reco_info = Streamer(reconPath + 'reco.sbc')
 reco_info = reco_info.to_dict()
 if args.log:
     print('[Log] Found reco.sbc')
-print(bubble_finder_info)
 
 ## TODO: pull out cam, pos, earliest frame
 """
@@ -56,16 +55,29 @@ print(bubble_finder_info)
     print earliest frame
 
 """
-indexOfFirstCam1=0
-print(f'Cam 1 earliest guess:\n Pos:\t{bubble_finder_info["pos"][0]}\nEarliest Frame:\t{bubble_finder_info["frame"][0]}')
-indexOfFirstCam2 = indexOfFirstCam1
-while  bubble_finder_info["cam"][indexOfFirstCam2] != 2:
-    indexOfFirstCam2 += 1
+
+
+def findEarliest(camNum):
+    n_minSoFar = 0
+    f_minSoFar = 100
+    for n in range(0,len(bubble_finder_info["frame"])):
+            if (bubble_finder_info["frame"][n] < f_minSoFar) and (bubble_finder_info["cam"][n] == camNum):
+                f_minSoFar = bubble_finder_info["frame"][n]
+                n_minSoFar = n
+    return n_minSoFar
+
+indexOfFirstCam1=findEarliest(1)
+print(bubble_finder_info["frame"][indexOfFirstCam1])
+print(f'Cam 1 earliest guess:\n Pos:\t{bubble_finder_info["pos"][indexOfFirstCam1]}\nEarliest Frame:\t{bubble_finder_info["frame"][indexOfFirstCam1]}')
+
+indexOfFirstCam2=findEarliest(2)
+print(bubble_finder_info["frame"][indexOfFirstCam2])
 print(f'Cam 2 earliest guess:\n Pos:\t{bubble_finder_info["pos"][indexOfFirstCam2]}\nEarliest Frame:\t{bubble_finder_info["frame"][indexOfFirstCam2]}')
-indexOfFirstCam3 = indexOfFirstCam2
-while  bubble_finder_info["cam"][indexOfFirstCam3] != 3:
-    indexOfFirstCam3 += 1
+
+indexOfFirstCam3=findEarliest(3)
+print(bubble_finder_info["frame"][indexOfFirstCam3])
 print(f'Cam 3 earliest guess:\n Pos:\t{bubble_finder_info["pos"][indexOfFirstCam3]}\nEarliest Frame:\t{bubble_finder_info["frame"][indexOfFirstCam3]}')
+
 
 if args.recon:
     exit()
@@ -77,29 +89,32 @@ if args.log:
 with tarfile.open(dataPath) as tar:
     tar.extractall(scratchPath)
     tar.close()
-## TODO: cycle through camera 1, then 2, then 3
-## in each one, allow the user to look for the first frame with a bubble
-## make sure you can see the coordinates
+
 imcam_1 = None
 imcam_2 = None
 imcam_3 = None
 if int(args.index) != -1:
-    indexToUse = str(args.index)
-    indexOfFirstCam1 = indexToUse
-    indexOfFirstCam2 = indexToUse
-    indexOfFirstCam3 = indexToUse
+    frameToUse = str(args.index)
+    indexOfFirstCam1 = frameToUse
+    indexOfFirstCam2 = frameToUse
+    indexOfFirstCam3 = frameToUse
+else:
+    indexOfFirstCam1 = bubble_finder_info["frame"][indexOfFirstCam1]
+    indexOfFirstCam2 = bubble_finder_info["frame"][indexOfFirstCam2]
+    indexOfFirstCam3 = bubble_finder_info["frame"][indexOfFirstCam3]
+
 if int(indexOfFirstCam1) <10:
-    imcam_1 = img.open(scratchPath+ args.event+"/0/cam1-img0"+indexOfFirstCam1+".png").convert("L")
+    imcam_1 = img.open(scratchPath+ args.event+"/0/cam1-img0"+str(indexOfFirstCam1)+".png").convert("L")
 else:
-    imcam_1 = img.open(scratchPath+ args.event+"/0/cam1-img"+indexOfFirstCam1+".png").convert("L")
+    imcam_1 = img.open(scratchPath+ args.event+"/0/cam1-img"+str(indexOfFirstCam1)+".png").convert("L")
 if int(indexOfFirstCam2) <10:
-    imcam_2 = img.open(scratchPath+ args.event+"/0/cam2-img0"+indexOfFirstCam2+".png").convert("L")
+    imcam_2 = img.open(scratchPath+ args.event+"/0/cam2-img0"+str(indexOfFirstCam2)+".png").convert("L")
 else:
-    imcam_2 = img.open(scratchPath+ args.event+"/0/cam2-img"+indexOfFirstCam2+".png").convert("L")
+    imcam_2 = img.open(scratchPath+ args.event+"/0/cam2-img"+str(indexOfFirstCam2)+".png").convert("L")
 if int(indexOfFirstCam3) <10:
-    imcam_3 = img.open(scratchPath+ args.event+"/0/cam3-img0"+indexOfFirstCam3+".png").convert("L")
+    imcam_3 = img.open(scratchPath+ args.event+"/0/cam3-img0"+str(indexOfFirstCam3)+".png").convert("L")
 else:
-    imcam_3 = img.open(scratchPath+ args.event+"/0/cam3-img"+indexOfFirstCam3+".png").convert("L")
+    imcam_3 = img.open(scratchPath+ args.event+"/0/cam3-img"+str(indexOfFirstCam3)+".png").convert("L")
 
 
 ## in theorey i should do the subtraction thing here but its fine.
@@ -111,20 +126,22 @@ if (args.log):
 # display
 fig = plt.figure()
 plt.imshow(imcam_1,cmap='grey')
-plt.title("Cam 1 for " + args.event + " at frame " + indexOfFirstCam1)
+plt.title("Cam 1 for " + args.event + " at frame " + str(indexOfFirstCam1))
 plt.show()
 
 plt.imshow(imcam_2,cmap='grey')
-plt.title("Cam 2 for " + args.event + " at frame " + indexOfFirstCam2)
+plt.title("Cam 2 for " + args.event + " at frame " + str(indexOfFirstCam2))
 plt.show()
 
 plt.imshow(imcam_3,cmap='grey')
-plt.title("Cam 3 for " + args.event + " at frame " + indexOfFirstCam3)
+plt.title("Cam 3 for " + args.event + " at frame " + str(indexOfFirstCam3))
 plt.show()
 
 # cleanup
 ## mostly just deleting the scratch dir to conserve disk space
+
 if (args.log):
     print("[Log] Cleaning up scratch directory.")
+
 shutil.rmtree(scratchPath)
 
