6af60c2f34c14b07467559d784ca40e7b24abb39
Olivia Chiarini <chiariniolivia@gmail.com>
2026-05-21 10:26:53 -0400

    updated neutron rate script with run list

--- graphing/neutronrate.py
+++ graphing/neutronrate.py
@@ -1,53 +1,70 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
-from openpyxl import load_workbook
+import sys, csv, glob, os
 
-def nth_col_numbers(path, n, exclude=None, runType=None,  sheet_name=None):
-    wb = load_workbook(path, data_only=True)
-    ws = wb[sheet_name] if sheet_name else wb.active    
-    nums = []
-    rowCount = None
-    if exclude is not None:
-        rowCount = []
-    i=0
-    for row in ws.iter_rows(min_row=(1), min_col=(0), max_col=(18), values_only=True):
-        if exclude is not None:
-            if i in exclude:
-                i+=1
-                continue
-            i+=1
-        if runType is not None:
-            if (row[0] is not None) and (row[16] is not None) and (runType not in row[16]):
-                continue
-
-        val = row[n]
-        if isinstance(val, (int, float)):
-            nums.append(val)
-        else:
-            try:
-                nums.append(float(val))
-            except (TypeError, ValueError):
-                pass
-    return nums
-
-
+# path to csv output from handscanning in LAr10Ana
 path = sys.argv[1]
 
-bubbleCount = nth_col_numbers(path,2)
+## warm annular
+neutronRunsWarm = ["20260107_3", "20260107_4", "20260107_5", "20260107_6", "20260107_7", "20260108_0", "20260108_1", "20260108_2", "20260108_3", "20260108_4", "20260108_5", "20260108_6", "20260108_7", "20260108_8", "20260109_0"]
+## cold annular
+neutronRunsCold = ["20260122_3","20260122_4","20260122_5","20260122_6","20260123_0","20260123_1","20260123_2","20260123_3","20260123_4","20260123_8","20260123_9","20260123_10","20260124_0","20260124_1","20260124_3","20260124_4","20260124_5","20260125_0","20260125_1","20260125_2","20260125_3","20260125_4","20260125_5","20260125_6","20260125_7","20260125_8"]
+
+## 119K
+neutronRunsHot = ["20260205_12","20260205_13","20260205_14","20260205_15","20260205_16","20260205_17","20260205_18","20260206_0","20260206_1","20260206_2","20260206_3","20260206_4","20260206_5","20260206_6","20260206_7","20260213_1","20260213_2","20260213_3","20260213_4","20260213_5","20260213_6","20260213_7","20260213_8","20260213_9","20260214_0","20260214_1","20260214_2","20260214_3","20260214_4","20260214_5","20260214_6","20260214_7","20260214_8","20260214_9","20260214_10","20260214_11","20260214_12","20260214_13","20260214_14","20260215_0","20260215_1","20260215_2","20260215_3","20260215_4","20260215_5","20260215_6","20260215_7","20260215_8","20260215_9","20260215_10","20260215_11","20260215_12","20260215_13","20260215_14","20260216_0","20260216_1","20260216_2","20260216_3","20260216_4","20260216_5","20260216_6","20260216_7","20260216_8","20260216_9","20260216_10","20260216_11","20260216_12","20260216_13","20260217_0","20260217_1","20260217_2","20260217_3","20260217_4","20260217_5","20260217_6"]
 
 
+## ones that are used for this graph
+neutronRuns = []
+for run in neutronRunsWarm:
+    neutronRuns.append(run)
+for run in neutronRunsCold:
+    neutronRuns.append(run)
+for run in neutronRunsHot:
+    neutronRuns.append(run)
+
+#neutronRuns.append("20260330_5")
+## purley for the sake of me testing
+
+def process_dir(dirpath):
+    checkedRuns = []
+    bubbleCount = []
+    for path in glob.glob(os.path.join(dirpath, "*.txt")):
+        with open(path, 'r', encoding='utf-8') as f:
+            for raw in f:
+                parts = raw.split()
+                if len(parts) < 5:
+                    continue
+                run = parts[0]
+                ev = parts[1]
+                try:
+                    mult = int(parts[4])
+                    region =int(parts[3])
+                except ValueError:
+                    continue
+                if run not in neutronRuns:
+                    checkedRuns.append((ev,run))
+                    continue
+                if (ev,run) in checkedRuns:
+                    continue
+                bubbleCount.append((mult,region))
+                checkedRuns.append((ev,run))
+    return bubbleCount
+
+bubbleCount = process_dir(path)
+
+# bin data and calc ratios 
 binLabels = ["1", "2", "3", "4", "5+"]
 binCounts = [0]*(len(binLabels))
+# idk if we need this but could help
+excludedRegions = []
 for n in bubbleCount:
-    try:
-        iN = int(n)
-    except Exception:
+    if n[1] in excludedRegions:
         continue
-    if iN >= 5:
+    if n[0] >= 5:
             binCounts[4] += 1
     else:
-        binCounts[iN-1] += 1
+        binCounts[n[0]-1] += 1
 
 binCountError = []
 binCountError.append(np.sqrt(0))
@@ -61,7 +78,7 @@ for i in range(0,len(binLabels)):
     ratioError.append( np.sqrt(np.abs( (binCountError[i]/binCounts[0])**2 + (binCounts[i] * binCountError[0]/(binCounts[0]**2))**2   )))
 
 
-## thresholds in eV, case B from ryan
+# thresholds in eV, case B from ryan
 thresholds = [0.0, 5000.0, 10000.0, 15000.0, 20000.0, 25000.0]
 ratiosSim = []
 simError  = []
@@ -76,12 +93,6 @@ simError.append([0.00549518, 0.00539489, 0.00464159, 0.00432533, 0.00378953, 0.0
 ratiosSim.append([0.03417694, 0.01363073, 0.01183152, 0.00800400, 0.00636605, 0.00657174])
 simError.append([0.00322163, 0.00264983, 0.00262369, 0.00218002, 0.00198435, 0.00205089])
 
-
-totalCounts = nth_col_numbers(path, 2)
-totalFinderCounts = nth_col_numbers(path, 3)
-
-
-
 simCountMin = []
 simCountMax = []
 for i in range(0,len(thresholds)-1):
@@ -91,6 +102,10 @@ for i in range(0,len(thresholds)-1):
         simCountMin[i].append(np.abs(binCounts[0]* (ratiosSim[j][i] - simError[j][i])))
         simCountMax[i].append(np.abs(binCounts[0] * (ratiosSim[j][i] + simError[j][i])))
 
+
+
+
+# graphing
 x = np.arange(len(binLabels))
 
 plt.figure(figsize=(16,9))
@@ -122,12 +137,8 @@ for i in range(num_groups):
                    edgecolor="black", label=f"{thresholds[i]/1000}KeV",zorder=2)
     shadedInBars.append(bars)
 
-
-
-
 points = x
 plt.errorbar(points, binCounts, yerr=binCountError,fmt='o',color="red", ecolor="red")
-
 
 plt.xticks(x,binLabels)
 plt.xlabel("Bubble Multiplicity",fontsize=16)
